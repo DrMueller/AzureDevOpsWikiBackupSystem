797a08823e48d5a073ee6967c60c970db60ba393
Matthias Müller <MatthiasM@Live.de>
2019-05-12 19:20:49 +0200

    WIP

--- Sources/Application/Areas/SubAreas/GitRepo/Services/Servants/Implementation/RepoPathServant.cs
+++ Sources/Application/Areas/SubAreas/GitRepo/Services/Servants/Implementation/RepoPathServant.cs
@@ -1,25 +1,33 @@
 ﻿using System.IO;
 using System.IO.Abstractions;
+using Mmu.Mlazh.AzureApplicationExtensions.Areas.Logging;
 
 namespace Mmu.AzureDevOpsWikiBackupSystem.Areas.SubAreas.GitRepo.Services.Servants.Implementation
 {
     public class RepoPathServant : IRepoPathServant
     {
         private readonly IFileSystem _fileSystem;
+        private readonly ILoggingService _loggingService;
 
-        public RepoPathServant(IFileSystem fileSystem)
+        public RepoPathServant(IFileSystem fileSystem, ILoggingService loggingService)
         {
             _fileSystem = fileSystem;
+            _loggingService = loggingService;
         }
 
         public string InitializeDownloadPath(string baseDirectory)
         {
             var fullPath = _fileSystem.Path.Combine(baseDirectory, "GitRepo");
+
+            _loggingService.LogInformation($"Initializing {fullPath}..");
+
             if (_fileSystem.Directory.Exists(fullPath))
             {
+                _loggingService.LogInformation($"Deleting subdirectories..");
                 DeleteDirectories(fullPath);
             }
 
+            _loggingService.LogInformation($"Creating directory..");
             var newDirectory = _fileSystem.Directory.CreateDirectory(fullPath);
             return newDirectory.FullName;
         }
